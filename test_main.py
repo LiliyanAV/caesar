ef06d45e1b7ca07731f0b04335a0c48444379717
aliaksandrochka <ivanova_sandra@inbox.ru>
2021-06-15 13:07:37 +0300

    added more tests

--- test_main.py
+++ test_main.py
@@ -4,5 +4,7 @@ from main import Caesar
 def test_crypt():
     c = Caesar()
     assert c.crypt("b arf") == "o nes"
-    assert c.crypt("b arf") == "o nes"
-    assert c.crypt("b arf") == "o nes"
+    assert c.crypt("b.arf") == "o.nes"
+    assert c.crypt("aaa*a**") == "nnn*n**"
+    assert c.crypt("aaa.)") == "nnn.)"
+    assert c.crypt(".....//////nnnnn***") == ".....//////aaaaa***"
